59e26d767fad65a1f2c32b7441f6754e5f3505a3
Chrisian Louria <tianlouria@gmail.com>
2019-02-16 14:14:28 -0500

    Added joining game buttons and pages

--- html/js/uno.js
+++ html/js/uno.js
@@ -2,7 +2,34 @@ $(document).ready(function(){
 	window.history.pushState({}, document.title, "/" + "");
 
 
+
+
+///Home Screen Button
 	$(document).on("click", "#hostGameButton", function(){
 		$("#mainContent").load("hostGame.html")
 	})
+
+	$(document).on("click", "#joinGameButton", function(){
+		$("#mainContent").load("joinGame.html")
+	})
+
+
+
+
+
+///Create or join game
+	$(document).on("click", "#createRequest", function(){
+		var gameName = $("#gameNameText").val()
+
+		/////socket
+
+	})
+
+	$(document).on("click", "#joinRequest", function(){
+		var gameName = $("#gameNameText").val()
+
+		//socket
+	})
+//////////
+
 })
