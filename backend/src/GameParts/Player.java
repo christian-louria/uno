1500ca77504e13e68a900f1b2f8338b73dc2a982
grant <millergrnt@gmail.com>
2019-02-16 14:26:03 -0500

    Wrote player and pile classes

--- backend/src/GameParts/Player.java
+++ backend/src/GameParts/Player.java
@@ -1,17 +1,28 @@
 package GameParts;
 
-import GameParts.Card;
-
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Player extends Socket {
 
     private ArrayList<Card> hand;
+    private String name;
 
-    Player() {
+    /**
+     * Player constructor
+     * @param numCards Number of cards to deal to player
+     * @param name Name of this player
+     */
+    Player( int numCards, String name ) {
 
+        Random r = new Random();
+        hand = new ArrayList<>();
+        this.name = name;
 
+        while(numCards-- > 0){
+            hand.add(new Card(r));
+        }
     }
 
     /**
@@ -19,4 +30,23 @@ public class Player extends Socket {
      * @return This player's hand
      */
     public ArrayList<Card> getHand() { return hand; }
+
+
+    /**
+     * Player toString
+     * @return String representing the Player object
+     */
+    @Override
+    public String toString() {
+
+        String ret = this.name + ": ";
+
+        for(Card c : this.hand) {
+
+            ret += c.toString();
+            ret += "  ";
+        }
+
+        return ret;
+    }
 }
