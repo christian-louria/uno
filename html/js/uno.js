2266f59fd221a12d8c484347073f584f3a3a5179
Chrisian Louria <tianlouria@gmail.com>
2019-02-16 14:49:28 -0500

    tried to add history

--- html/js/uno.js
+++ html/js/uno.js
@@ -1,16 +1,40 @@
-$(document).ready(function(){
-	window.history.pushState({}, document.title, "/" + "");
+function index(){
+	$("#mainContent").load("home.html");
+}
 
+function hostGame(){
+	$("#mainContent").load("hostGame.html");
+}
+
+function joinGame(){
+	$("#mainContent").load("joinGame.html");
+}
+
+
+$(document).ready(function(){
+	window.history.pushState('forward', null, './index');
+	
+	if (window.history && window.history.pushState) {
+
+    window.history.pushState('forward', null, './#forward');
+
+    $(window).on('popstate', function() {
+      alert('Back button was pressed.');
+    });
+
+  }
 
 
 
 ///Home Screen Button
 	$(document).on("click", "#hostGameButton", function(){
-		$("#mainContent").load("hostGame.html")
+		window.history.pushState('forward', null, './hostGame');
+		hostGame();
 	})
 
 	$(document).on("click", "#joinGameButton", function(){
-		$("#mainContent").load("joinGame.html")
+		window.history.pushState('forward', null, './joinGame');
+		joinGame();
 	})
 
 
